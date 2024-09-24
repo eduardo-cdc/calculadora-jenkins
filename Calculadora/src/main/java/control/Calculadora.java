dc5dce200dd51ec97e5bcfd75f49dd40524ff699
Eduardo Costa <117341782+eduardo-cdc@users.noreply.github.com>
2024-09-24 20:18:18 -0300

    Add files via upload

---
+++ Calculadora/src/main/java/control/Calculadora.java
@@ -0,0 +1,7 @@
+package control;
+
+public class Calculadora {
+    public double soma(double valor1, double valor2){
+        return valor1 + valor2;
+    }
+}
---
+++ Calculadora/src/test/java/tests/CalculadoraTests.java
@@ -0,0 +1,14 @@
+package tests;
+
+import control.Calculadora;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculadoraTests {
+    @Test
+    public void TesteSomar(){
+        Calculadora calc = new Calculadora();
+
+        Assert.assertEquals(5.0,calc.soma(3.0,2.0),5.0);
+    }
+}
